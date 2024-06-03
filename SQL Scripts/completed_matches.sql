1909f3d059756cede5e4f170647d6e538f900b12
kevin ocansey <kevinocansey11@gmail.com>
2024-06-03 13:00:45 +0000

    Testing all my models, cleaned up with logistic regression, cleaned up with comparing classfiers. The next step is working on making sure evrything is automated before the next premeier league starts. This has been exciting. Whats next is probaly removing my passwords and sharing my work with the public!

--- SQL Scripts/completed_matches.sql
+++ SQL Scripts/completed_matches.sql
@@ -1,31 +1,35 @@
 
-drop table bet_prediction_model.forebet_prediction_table;
 
-CREATE TABLE bet_prediction_model.forebet_prediction_table (
-    Home INT,
-    Away INT,
-    Home_Win_Probability DOUBLE,
-    Draw_Probability DOUBLE,
-    Away_Team_Win_Probability DOUBLE,
-    Team_To_Win_Prediction INT,
-    Average_Goals_Prediction DOUBLE,
-    Weather_In_Degrees TEXT,
-    Odds DOUBLE,
-    Full_Time_Score TEXT,
-    Score_At_Halftime TEXT,
-    Date DATETIME,
-    Time TIME,
-    Home_Team_Score_Prediction INT,
-    Away_Team_Score_Prediction INT,
-    Home_Team_Full_Time_Score INT,
-    Away_Team_Full_Time_Score INT,
-    Home_Team_Halftime_Score INT,
-    Away_Team_Halftime_Score INT,
-    Prediction_Result_Won_Loss INT,
-    Day_Of_Week INT,
-    Month INT,
-    Weekly_Round INT
+-- drop table  <nameofdb>.<nameoftable>;
+
+
+CREATE TABLE <nameofdb>.<nameoftable> (
+    home text,
+    away text,
+    home_win_Probability DOUBLE,
+    draw_probability DOUBLE,
+    away_win_probability DOUBLE,
+    team_to_win_prediction INT,
+    average_goals_prediction DOUBLE,
+    weather_in_degrees TEXT,
+    odds DOUBLE,
+    full_time_score TEXT,
+    score_at_halftime TEXT,
+    date DATETIME,
+    time TEXT, --I used time initially but when i push my data into the db it will change to text so might as well just leave it as such
+    home_team_score_prediction INT,
+    away_team_score_prediction INT,
+    home_team_full_time_score INT,
+    away_team_full_time_score INT,
+    home_team_halftime_score INT,
+    away_team_halftime_score INT,
+    prediction_result INT,
+    day_of_week INT,
+    month INT,
+    weekly_round INT
 );
+
+
 INSERT INTO bet_prediction_model.forebet_prediction_table
 SELECT * FROM sandbox.forebet_prediction_table;
 
